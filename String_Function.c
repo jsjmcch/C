8a75c795691172fceda17b7ace5b7ef62088ec17
Milller.Jo <jsjmcch@gmail.com>
2017-08-24 08:22:45 +0900

    Update String_Function.c

--- String_Function.c
+++ String_Function.c
@@ -1869,7 +1869,24 @@ int main ()
 isxdigit is used to check if the first character in str is a valid hexadecimal digit and therefore a valid candidate to be converted by strtol into an integral value. Output:
 The hexadecimal number ffff is 65535.
 ==============================================================================
+/* strtol example */
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* strtol */
 
+int main ()
+{
+  char szNumbers[] = "2001 60c0c0 -1101110100110100100000 0x6fffff";
+  char * pEnd;
+  long int li1, li2, li3, li4;
+  li1 = strtol (szNumbers,&pEnd,10);
+  li2 = strtol (pEnd,&pEnd,16);
+  li3 = strtol (pEnd,&pEnd,2);
+  li4 = strtol (pEnd,NULL,0);
+  printf ("The decimal equivalents are: %ld, %ld, %ld and %ld.\n", li1, li2, li3, li4);
+  return 0;
+}
+Output:
+The decimal equivalents are: 2001, 6340800, -3624224 and 7340031
 ==============================================================================
 
 ==============================================================================
